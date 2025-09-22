8099678c6869a2de054c9b62d5b06dd3f85cb838
JHANVIN <nagorijhanvi@gmail.com>
2025-09-22 23:13:26 +0530

    resolving cart issue

--- components/Cart.js
+++ components/Cart.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Cart() {
   return (
-    <img src="/logos/cart.svg" alt="Cart" width={31} height={34} />
+    <Image src="logos/cart.svg" alt="Cart" width={31} height={34} />
+
   );
 }
